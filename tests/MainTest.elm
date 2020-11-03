0e4b33e20981f2412b3cda7e041bc0ee0ed4c11c
Nicholas Reynolds <nicholasreynolds.lambda@gmail.com>
2020-11-03 09:50:14 -0800

    Initialize project

---
+++ tests/MainTest.elm
@@ -0,0 +1,7 @@
+module MainTest exposing (..)
+
+import Test exposing (..)
+
+suite : Test
+suite =
+    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
